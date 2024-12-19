365015c287d1c27c130ddb851a6685fef31e5aa1
huniversal <hunivers0523@naver.com>
2024-12-19 17:16:50 +0900

    Feat: 컴포넌트별 페이지 구성

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h3>푸터</h3>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/SignPage/SignPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignPage = () => {
+  return <div></div>;
+};
+
+export default SignPage;
