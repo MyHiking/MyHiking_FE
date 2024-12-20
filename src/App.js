34b6bf77d98cd4c11a1e21fbff55dad39294639a
huniversal <hunivers0523@naver.com>
2024-12-20 12:09:14 +0900

    Feat: 로그인, 회원가입 페이지 접속시 Head Hide 로그인 및 회원가입 페이지에 접속했을때 Header 안보에기 구성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import "./App.css";
 
-//컴포넌트 import
+// 컴포넌트 import
 import MainPage from "./components/MainPage/MainPage";
 import Community from "./components/Community/Community";
 import DashBoard from "./components/DashBoard/DashBoard";
@@ -10,23 +15,33 @@ import Header from "./components/Layout/Header";
 import LoginPage from "./components/LoginPage/LoginPage";
 import SignPage from "./components/SignPage/SignPage";
 
+function AppContent() {
+  const location = useLocation();
+
+  // 로그인 및 회원가입 페이지에서 Header 숨기기
+  const hideHeader = ["/login", "/sign"].includes(location.pathname);
+
+  return (
+    <div className="App">
+      {!hideHeader && <Header />}
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/com" element={<Community />} />
+          <Route path="/dash" element={<DashBoard />} />
+          <Route path="/my" element={<MyPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/sign" element={<SignPage />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <main className="main">
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/com" element={<Community />} />
-            <Route path="/dash" element={<DashBoard />} />
-            <Route path="/my" element={<MyPage />} />
-
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/sign" element={<SignPage />} />
-          </Routes>
-        </main>
-      </div>
+      <AppContent />
     </Router>
   );
 }
