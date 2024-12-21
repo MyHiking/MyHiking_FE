c0e23c7380b454de41d9e554a99dd31345cfc940
huniversal <hunivers0523@naver.com>
2024-12-21 19:45:23 +0900

    Feat: 회원가입 폼 html 구성

---
+++ src/components/SignPage/SignHeader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Style/signheader.scss";
+
+const SignHeader = () => {
+  return (
+    <header className="sign-header">
+      <div className="header-logo">
+        <h2>🏔️MyHiking</h2>
+      </div>
+    </header>
+  );
+};
+
+export default SignHeader;
--- src/components/SignPage/SignPage.jsx
+++ src/components/SignPage/SignPage.jsx
@@ -1,9 +1,56 @@
 import React from "react";
+import "../Style/signpage.scss";
+import SignHeader from "./SignHeader";
 
 const SignPage = () => {
   return (
-    <div>
-      <h3>SignPage</h3>
+    <div className="signPage">
+      <SignHeader />
+      <div className="sign-content">
+        <h2>회원가입</h2>
+        <form className="register-form">
+          <div className="form-group">
+            <label>이메일</label>
+            <div className="email-input">
+              <input type="email" placeholder="이메일" />
+              <button type="button">인증</button>
+            </div>
+            <input className="text" placeholder="인증번호 입력" />
+          </div>
+
+          <div className="form-group">
+            <label>비밀번호</label>
+            <input type="password" placeholder="비밀번호 입력" />
+            <input type="password" placeholder="비밀번호 확인" />
+          </div>
+
+          <div className="form-group">
+            <label>닉네임</label>
+            <div className="agreement">
+              <div>
+                <input type="checkbox" id="all-agree" />
+                <label htmlFor="all-agree">전체동의</label>
+              </div>
+              <div>
+                <input type="checkbox" id="terms" />
+                <label htmlFor="terms">이용약관</label>
+              </div>
+              <div>
+                <input type="checkbox" id="privacy" />
+                <label htmlFor="privacy">개인정보 수집 및 이용동의</label>
+              </div>
+              <div>
+                <input type="checkbox" id="marketing" />
+                <label htmlFor="marketing">마케팅 이메일 수신</label>
+              </div>
+            </div>
+          </div>
+
+          <button type="submit" className="register-btn">
+            회원가입
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
